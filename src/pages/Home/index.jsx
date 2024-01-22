672c3ed746528d76b6194cfee12c4d3f03d8427e
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-01-22 10:49:47 +0100

    add config

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/react-in-jsx-scope */
+function Home() {
+    // eslint-disable-next-line react/no-unescaped-entities
+    return <div>Page d'accueil 🏡</div>
+  }
+  
+  export default Home
